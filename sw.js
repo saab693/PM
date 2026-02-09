a3cd38db9b8f7ff136c8fcda5c22ce73792b3a9e
saab693 <saab693@gmail.com>
2026-02-09 13:09:29 +0000

    Add service worker with install, activate, and fetch events

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {});
